65c6c8d7bbbb65a91a9be819e4f74834247c2f35
CRT_HAO <m831718@gmail.com>
2023-11-13 01:50:26 +0800

    add test script

---
+++ scripts/test.sh
@@ -0,0 +1,12 @@
+cd ../build/
+./state_minimization ../examples/set00/case1/input.kiss ../examples/set00/case1/output.kiss ../examples/set00/case1/output.dot
+
+./state_minimization ../examples/set01/case1/input.kiss ../examples/set01/case1/output.kiss ../examples/set01/case1/output.dot
+./state_minimization ../examples/set01/case2/input.kiss ../examples/set01/case2/output.kiss ../examples/set01/case2/output.dot
+./state_minimization ../examples/set01/case3/input.kiss ../examples/set01/case3/output.kiss ../examples/set01/case3/output.dot
+./state_minimization ../examples/set01/case4/input.kiss ../examples/set01/case4/output.kiss ../examples/set01/case3/output.dot
+./state_minimization ../examples/set01/case5/input.kiss ../examples/set01/case5/output.kiss ../examples/set01/case3/output.dot
+
+./state_minimization ../examples/set02/case1/input.kiss ../examples/set01/case1/output.kiss ../examples/set01/case1/output.dot
+./state_minimization ../examples/set02/case2/input.kiss ../examples/set01/case2/output.kiss ../examples/set01/case2/output.dot
+./state_minimization ../examples/set02/case3/input.kiss ../examples/set01/case3/output.kiss ../examples/set01/case2/output.dot
