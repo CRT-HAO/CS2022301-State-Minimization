7db9bfe8b8ec9eee79a5ab15450ac06662081ca1
CRT_HAO <m831718@gmail.com>
2023-11-29 02:39:03 +0800

    fix wrong input/output paths

--- scripts/test.sh
+++ scripts/test.sh
@@ -1,12 +1,13 @@
 cd ../build/
 ./state_minimization ../examples/set00/case1/input.kiss ../examples/set00/case1/output.kiss ../examples/set00/case1/output.dot
+./state_minimization ../examples/set00/case2/input.kiss ../examples/set00/case2/output.kiss ../examples/set00/case2/output.dot
 
 ./state_minimization ../examples/set01/case1/input.kiss ../examples/set01/case1/output.kiss ../examples/set01/case1/output.dot
 ./state_minimization ../examples/set01/case2/input.kiss ../examples/set01/case2/output.kiss ../examples/set01/case2/output.dot
 ./state_minimization ../examples/set01/case3/input.kiss ../examples/set01/case3/output.kiss ../examples/set01/case3/output.dot
-./state_minimization ../examples/set01/case4/input.kiss ../examples/set01/case4/output.kiss ../examples/set01/case3/output.dot
-./state_minimization ../examples/set01/case5/input.kiss ../examples/set01/case5/output.kiss ../examples/set01/case3/output.dot
+./state_minimization ../examples/set01/case4/input.kiss ../examples/set01/case4/output.kiss ../examples/set01/case4/output.dot
+./state_minimization ../examples/set01/case5/input.kiss ../examples/set01/case5/output.kiss ../examples/set01/case5/output.dot
 
-./state_minimization ../examples/set02/case1/input.kiss ../examples/set01/case1/output.kiss ../examples/set01/case1/output.dot
-./state_minimization ../examples/set02/case2/input.kiss ../examples/set01/case2/output.kiss ../examples/set01/case2/output.dot
-./state_minimization ../examples/set02/case3/input.kiss ../examples/set01/case3/output.kiss ../examples/set01/case2/output.dot
+./state_minimization ../examples/set02/case1/input.kiss ../examples/set02/case1/output.kiss ../examples/set02/case1/output.dot
+./state_minimization ../examples/set02/case2/input.kiss ../examples/set02/case2/output.kiss ../examples/set02/case2/output.dot
+./state_minimization ../examples/set02/case3/input.kiss ../examples/set02/case3/output.kiss ../examples/set02/case3/output.dot
